0e20c48c6505ee9ea3410ba098adc531bbf216cd
antovegav <anto.vegav@duocuc.cl>
2023-10-27 19:03:57 -0300

    v 1.4.0

--- registrApp/src/environments/environment.ts
+++ registrApp/src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  supabaseUrl: "https://oroiqrkclheidjblybhg.supabase.co",
+  supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9yb2lxcmtjbGhlaWRqYmx5YmhnIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTcyODUxNDEsImV4cCI6MjAxMjg2MTE0MX0.PtGpacRGqBmLIQC5uYo_bvOQBGEGHbAspd3q1cpCpyo"
 };
 
 /*
