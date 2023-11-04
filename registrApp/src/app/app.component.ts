b572778473d6e733559c5a19cd0a623f582e734d
Ana G <ana.gonzalezr@duocuc.cl>
2023-11-04 08:31:34 -0300

    v 2.0.2 | diseño

--- registrApp/src/app/app.component.ts
+++ registrApp/src/app/app.component.ts
@@ -12,44 +12,46 @@ import { firstValueFrom } from 'rxjs';
 })
 export class AppComponent {
   // ARREGLO CON LAS PAGINAS QUE SE MOSTRARAN EN EL MENU
-  // public appPages = [
-  //   // { title: 'Inicio', url: 'estudiante', icon: 'home' },
-  //   { title: 'Crear Clase', url: 'crear-clase', icon: 'create' },
-  //   { title: 'Cambiar Contraseña', url: 'password-olvidado', icon: 'person-circle' },
-  //   { title: 'Salir', url: 'login', icon: 'log-in' },
-  // ];
-  userType: any;
-
-  router: Router;
-
-  appPages: any = [];
-
-  async ngOnInit(){
-    this.userType = await firstValueFrom(this.userType.getEstudiantes())
-  }
-  // ARREGLO DE LAS ETIQUETAS QUE SE MOSTRARAN EN EL MENU
-  constructor() {
-
-    this.roleMenu(this.userType);
-  }
-
-  private roleMenu(userType: string){
-    this.appPages = [];
-  
-    if(userType = 'estudiante'){
-      this.appPages.push(
-        { title: 'Inicio', url: 'estudiante', icon: 'home' },
-        // { title: 'Escanear', url: 'inicio', icon: 'create' },
-        { title: 'Cambiar Contraseña', url: 'password-olvidado', icon: 'person-circle' },
-        { title: 'Salir', url: 'login', icon: 'log-in' }
-      );
-    }else if (userType == 'profesor'){
-        this.appPages.push(
-          { title: 'Inicio', url: 'profesor', icon: 'home' },
-          { title: 'Crear Clase', url: 'crear-clase', icon: 'create' },
-          { title: 'Cambiar Contraseña', url: 'password-olvidado', icon: 'person-circle' },
-          { title: 'Salir', url: 'login', icon: 'log-in' }
-        )
-    };
-  }
+  public appPages = [
+    { title: 'Inicio', url: 'estudiante', icon: 'home' },
+    // { title: 'Crear Clase', url: 'crear-clase', icon: 'create' },
+    { title: 'Cambiar Contraseña', url: 'password-olvidado', icon: 'person-circle' },
+    { title: 'Salir', url: 'login', icon: 'log-in' },
+  ];
 }
+
+//   userType: any;
+
+//   router: Router;
+
+//   appPages: any = [];
+
+//   async ngOnInit(){
+//     this.userType = await firstValueFrom(this.userType.getEstudiantes())
+//   }
+//   // ARREGLO DE LAS ETIQUETAS QUE SE MOSTRARAN EN EL MENU
+//   constructor() {
+
+//     this.roleMenu(this.userType);
+//   }
+
+//   private roleMenu(userType: string){
+//     this.appPages = [];
+  
+//     if(userType = 'estudiante'){
+//       this.appPages.push(
+//         { title: 'Inicio', url: 'estudiante', icon: 'home' },
+//         // { title: 'Escanear', url: 'inicio', icon: 'create' },
+//         { title: 'Cambiar Contraseña', url: 'password-olvidado', icon: 'person-circle' },
+//         { title: 'Salir', url: 'login', icon: 'log-in' }
+//       );
+//     }else if (userType == 'profesor'){
+//         this.appPages.push(
+//           { title: 'Inicio', url: 'profesor', icon: 'home' },
+//           { title: 'Crear Clase', url: 'crear-clase', icon: 'create' },
+//           { title: 'Cambiar Contraseña', url: 'password-olvidado', icon: 'person-circle' },
+//           { title: 'Salir', url: 'login', icon: 'log-in' }
+//         )
+//     };
+//   }
+// }
