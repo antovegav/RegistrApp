20de7c05f66b6634726622145eaf18028e9b8b54
antovegav <anto.vegav@duocuc.cl>
2023-11-30 04:03:02 -0300

    v 2.2.0

--- registrApp/src/app/inicio/inicio.page.ts
+++ registrApp/src/app/inicio/inicio.page.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { ObtenerUsuarioService } from '../services/obtener-usuario.service';
+import { Barcode, BarcodeScanner } from '@capacitor-mlkit/barcode-scanning';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-inicio',
@@ -17,9 +19,38 @@ export class InicioPage implements OnInit {
 
   userInfoReceived: any;
 
-  constructor(private obtenerUsuarioService: ObtenerUsuarioService) { }
+  isSupported = false;
+  barcodes: Barcode[] = [];
+
+  constructor(private alertController: AlertController) {}
 
   ngOnInit() {
+    BarcodeScanner.isSupported().then((result) => {
+      this.isSupported = result.supported;
+    });
   }
 
+  async scan(): Promise<void> {
+    const granted = await this.requestPermissions();
+    if (!granted) {
+      this.presentAlert();
+      return;
+    }
+    const { barcodes } = await BarcodeScanner.scan();
+    this.barcodes.push(...barcodes);
+  }
+
+  async requestPermissions(): Promise<boolean> {
+    const { camera } = await BarcodeScanner.requestPermissions();
+    return camera === 'granted' || camera === 'limited';
+  }
+
+  async presentAlert(): Promise<void> {
+    const alert = await this.alertController.create({
+      header: 'Permission denied',
+      message: 'Please grant camera permission to use the barcode scanner.',
+      buttons: ['OK'],
+    });
+    await alert.present();
+  }
 }
